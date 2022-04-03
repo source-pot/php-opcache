bdee87ca074d3240f8a7f21bd629461c2045a87a
Rob Watson <rob.watson@me.com>
2022-04-03 11:42:39 +0100

    Remove config static class

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Config {
-    public const DB_USER = "demo_user";
-    public const DB_PASS = "secure_password";
-    public const DB_DATABASE = "demo";
-    public const DB_PORT = 3306;
-    public const DB_HOST = "mysql";
-}
--- src/html/index.php
+++ src/html/index.php
@@ -1,9 +1,10 @@
 <?php
 
 use SourcePot\Http\Response;
-use SourcePot\Database;
+use SourcePot\Database\Database;
 
+$query = 'SHOW DATABASES';
 $db = Database::getInstance();
-$stmt = $db->run('SHOW DATABASES');
-$databases = json_encode($stmt->fetchAll());
-(new Response)->setHeader('content-type', 'application/json')->send($databases);
+(new Response)
+    ->setHeader('content-type', 'application/json')
+    ->send($db->run($query));
